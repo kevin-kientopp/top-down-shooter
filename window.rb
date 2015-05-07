def4d024156a4b9cfe28454537c1f6e1638732e5
John <john.mccarthy645@gmail.com>
2015-05-07 17:50:45 -0600

    player now walks with animation!!!

--- window.rb
+++ window.rb
@@ -6,7 +6,7 @@ class Window < Gosu::Window
     super WIDTH, HEIGHT, false
     self.caption = 'Sexy Vampire Shooter'
 
-    @player = Player.new(Gosu::Image.new(self, 'media/player.png', true))
+    @player = Player.new(Gosu::Image.load_tiles(self, 'media/player.png', 16, 21, true))
     @player.warp(Level::WIDTH/2.0, Level::HEIGHT/2.0)
 
     tile_images = Array.new
