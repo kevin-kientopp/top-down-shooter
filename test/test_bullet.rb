97d78716779237acf8207b2fa5919e413d89b359
Kevin Kientopp <kevinkientopp@gmail.com>
2015-05-07 23:03:45 -0600

    Added more tests.

--- test/test_bullet.rb
+++ test/test_bullet.rb
@@ -8,4 +8,25 @@ class TestBullet < Test::Unit::TestCase
 
     assert_equal(7, bullet.y)
   end
+
+  def test_move_up
+    bullet = Bullet.new(0, 5, 180, nil)
+    bullet.move
+
+    assert_equal(3, bullet.y)
+  end
+
+  def test_move_left
+    bullet = Bullet.new(5, 0, 90, nil)
+    bullet.move
+
+    assert_equal(3, bullet.x)
+  end
+
+  def test_move_right
+    bullet = Bullet.new(5, 0, 270, nil)
+    bullet.move
+
+    assert_equal(7, bullet.x)
+  end
 end
