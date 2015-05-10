e91a42020a3610519b9e0ef55b067d081721b3ee
Kevin Kientopp <kevinkientopp@gmail.com>
2015-05-09 23:03:50 -0600

    Added tests.

--- test/test_player.rb
+++ test/test_player.rb
@@ -8,4 +8,53 @@ class TestPlayer < Minitest::Test
 
     assert_equal(-Player::VELOCITY, player.y)
   end
+
+  def test_move_down
+    player = Player.new nil
+    player.move_down
+
+    assert_equal(Player::VELOCITY, player.y)
+  end
+
+  def test_move_right
+    player = Player.new nil
+    player.move_right
+
+    assert_equal(Player::VELOCITY, player.x)
+  end
+
+  def test_move_left
+    player = Player.new nil
+    player.move_left
+
+    assert_equal(-Player::VELOCITY, player.x)
+  end
+
+  def test_move_up_angle
+    player = Player.new nil
+    player.move_up
+
+    assert_equal(0, player.angle)
+  end
+
+  def test_move_down_angle
+    player = Player.new nil
+    player.move_down
+
+    assert_equal(180, player.angle)
+  end
+
+  def test_move_right_angle
+    player = Player.new nil
+    player.move_right
+
+    assert_equal(90, player.angle)
+  end
+
+  def test_move_left_angle
+    player = Player.new nil
+    player.move_left
+
+    assert_equal(270, player.angle)
+  end
 end
